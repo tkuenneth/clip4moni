e8204f7e2972845e61b439e85fc6f5964653e55f
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-01-12 16:20:18 +0100

    fixed some warnings

--- src/com/thomaskuenneth/clip4moni/ShowContentsDialog.java
+++ src/com/thomaskuenneth/clip4moni/ShowContentsDialog.java
@@ -3,7 +3,7 @@
  * 
  * This file is part of Clip4Moni.
  * 
- * Copyright (C) 2015  Thomas Kuenneth
+ * Copyright (C) 2015 - 2018  Thomas Kuenneth
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2
@@ -22,7 +22,6 @@ package com.thomaskuenneth.clip4moni;
 
 import java.awt.BorderLayout;
 import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
@@ -34,8 +33,15 @@ import javax.swing.JTextArea;
  * @author thomas
  */
 public class ShowContentsDialog extends AbstractDialog {
-    
+
+    private final String contents;
+
     public ShowContentsDialog(final String contents) {
+        this.contents = contents;
+        init();
+    }
+
+    private void init() {
         // text area
         final JTextArea textareaContents = new JTextArea(contents);
         textareaContents.setEditable(false);
@@ -45,23 +51,19 @@ public class ShowContentsDialog extends AbstractDialog {
         add(new JScrollPane(textareaContents), BorderLayout.CENTER);
         // checkbox
         final JCheckBox checkboxDecode = new JCheckBox(Messages.getString("CHECKBOX_DECODE"));
-        checkboxDecode.addItemListener(new ItemListener() {
-            
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                String s = checkboxDecode.isSelected() ? StringUtils.decode(contents) : contents;
-                textareaContents.setText(s);
-            }
+        checkboxDecode.addItemListener((ItemEvent e) -> {
+            String s = checkboxDecode.isSelected() ? StringUtils.decode(contents) : contents;
+            textareaContents.setText(s);
         });
         add(checkboxDecode, BorderLayout.NORTH);
         checkboxDecode.setSelected(false);
     }
-    
+
     @Override
     public String getTitle() {
         return Messages.getString("TITLE_SHOW_CONTENTS");
     }
-    
+
     @Override
     public int getOption() {
         return JOptionPane.PLAIN_MESSAGE;
