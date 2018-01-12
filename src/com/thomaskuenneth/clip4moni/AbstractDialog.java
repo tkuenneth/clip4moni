a4381c1523fe6487444d3236b3d589f18c93229a
Thomas Künneth <thomas.kuenneth@mathema.de>
2018-01-12 16:00:17 +0100

    fixed a warning

--- src/com/thomaskuenneth/clip4moni/AbstractDialog.java
+++ src/com/thomaskuenneth/clip4moni/AbstractDialog.java
@@ -3,7 +3,7 @@
  * 
  * This file is part of Clip4Moni.
  * 
- * Copyright (C) 2013  Thomas Kuenneth
+ * Copyright (C) 2013 - 2018  Thomas Kuenneth
  *
  * Clip4Moni is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2
@@ -27,6 +27,10 @@ import javax.swing.JPanel;
 public abstract class AbstractDialog extends JPanel {
 
     public AbstractDialog() {
+        init();
+    }
+
+    private void init() {
         setLayout(new BorderLayout());
     }
 
