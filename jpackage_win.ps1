4825ed39fb99bfc9b84a586343d29df71b47b37c
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2020-07-25 12:01:37 +0200

    Update jpackage_win.ps1

--- jpackage_win.ps1
+++ jpackage_win.ps1
@@ -1,18 +1,21 @@
-#!/bin/sh
+$java_home = "C:\Program Files\Java\jdk-14.0.2"
+$base_dir = "C:\Users\tkuen\Entwicklung\Bitbucket\clip4moni"
+$version = "???"
 
-$JAVA_HOME = "C:\Program Files\Java\jdk-14.0.2"
-$BASEDIR = "C:\Users\tkuen\Entwicklung\Bitbucket\clip4moni"
-$VERSION = "1.3.3" # `sed -n -e 's/.*VERSION = \"\(.*\)\".*/\1/p' < $BASEDIR/src/com/thomaskuenneth/Clip4Moni/Main.java`
+$source = Get-Content -Path $base_dir\src\main\classes\com\thomaskuenneth\clip4moni\Clip4Moni.java
+foreach($line in $source) {
+    if ($line -match "version = `"(.+)`"") {
+        $version = $matches[1]
+        break
+    }
+}
 
-Write-Output "JAVA_HOME: $JAVA_HOME"
-Write-Output "BASEDIR: $BASEDIR"
-Write-Output "Version: $VERSION"
+Write-Output "java_home: $java_home"
+Write-Output "base_dir: $base_dir"
+Write-Output "version: $version"
 
-#rmdir -r "$BASEDIR\Clip4Moni"
+$command = "$java_home\bin\jpackage.exe"
+$arguments = "--win-menu --win-menu-group `"Thomas Kuenneth`" --vendor `"Thomas Kuenneth`" --name Clip4Moni --icon $base_dir\artwork\Clip4Moni.ico --type msi --app-version $version --module-path $base_dir\dist\main.jar;`"C:\Program Files\Java\javafx-jmods-13.0.2`" -m main/com.thomaskuenneth.clip4moni.Clip4Moni"
 
-$COMMAND = "$JAVA_HOME\bin\jpackage.exe"
-$ARGUMENTS = "--win-menu --win-menu-group `"Thomas Kuenneth`" --vendor `"Thomas Kuenneth`" --name Clip4Moni --icon $BASEDIR\artwork\Clip4Moni.ico --type msi --app-version $VERSION --module-path $BASEDIR\dist\main.jar;`"C:\Program Files\Java\javafx-jmods-13.0.2`" -m main/com.thomaskuenneth.clip4moni.Clip4Moni"
-
-Write-Output $ARGUMENTS
-
-Start-Process -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -FilePath $COMMAND -ArgumentList $ARGUMENTS -Wait
+Write-Output $arguments
+Start-Process -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -FilePath $command -ArgumentList $arguments -Wait
